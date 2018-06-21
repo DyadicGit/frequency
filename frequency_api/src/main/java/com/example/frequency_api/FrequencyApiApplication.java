5b339986ccf36ae2e95e389c4cc7e68bced7e734
Olgerd Ševiakov <olgerd@zenitech.co.uk>
2018-06-21 10:39:59 +0300

    first commit

---
+++ frequency_api/src/main/java/com/example/frequency_api/FrequencyApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.frequency_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FrequencyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrequencyApiApplication.class, args);
+    }
+}
