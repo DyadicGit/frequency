577c65021a66962fa7bedf996a8c07d14974b9de
Olgerd Ševiakov <olgerd@zenitech.co.uk>
2018-07-03 09:27:12 +0300

    fallback in feign

--- frequency-api/src/main/java/org/zenitech/frequencyapi/client/FrequencyServiceClient.java
+++ frequency-api/src/main/java/org/zenitech/frequencyapi/client/FrequencyServiceClient.java
@@ -1,13 +1,22 @@
 package org.zenitech.frequencyapi.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import reactor.core.publisher.Mono;
 
-@FeignClient("frequency-service")
+@FeignClient(value = "frequency-service", fallback = FrequencyServiceClient.Fallback.class)
 public interface FrequencyServiceClient {
 
     @PostMapping("/process/{text}")
     String process(@PathVariable("text") String text);
+
+    @Service
+    class Fallback implements FrequencyServiceClient {
+        @Override
+        public String process(final String text) {
+            return "Fallback"+text;
+        }
+    }
 }
